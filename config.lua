a32d340d354f4c3bf0e98f16ca8566cc24eb8ec5
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-21 02:28:26 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+ConcessMoto             = {}
+
+ConcessMoto.webhooks = "TON WEEBHOOK ICI"
+
+ConcessMoto.jeveuxmarker = true --- true = Oui | false = Non
+
+ConcessMoto.jeveuxblips = true --- true = Oui | false = Non
+
+ConcessMoto.voiture = true --- true = Si vous utiliser le fConcess | false = non
+
+ConcessMoto.pos = {
+	blip = {
+		position = {x = 286.8, y = -1146.12, z = 29.29}
+	},
+    menu = {
+        position = {x = 294.12, y = -1169.29, z = 29.67}
+    },
+    boss = {
+        position = {x = 274.18, y = -1152.22, z = 33.27}
+    },
+    serrurier = {
+        position = {x = 271.89, y = -1168.13, z = 29.28}
+    },
+    spawnmoto = {
+        position = {x = 276.48, y = -1159.51, z = 28.78, h = 92.8}
+    },
+}
